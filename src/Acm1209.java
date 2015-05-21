9ab6a854ea1230ba38e52763683b41c5423a7236
unknown <meherzad4u@rediffmail.com>
2015-05-21 22:53:13 +0530

    20150521-1

---
+++ src/Acm1209.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Acm1209 {
+	private static BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws IOException {
+		int num, n = Integer.parseInt(bReader.readLine());
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < n; i++) {
+			num = Integer.parseInt(bReader.readLine());
+			double t = 2 * (num - 1);
+			double sqr = Math.floor(Math.sqrt(t));
+
+			if (sqr * (sqr + 1) == t) {
+				sb.append(1).append(" ");
+			} else {
+				sb.append(0).append(" ");
+			}
+		}
+		System.out.println(sb.toString().trim());
+	}
+}
